027d8da4577069887c9290deef5fadb695d5cafd
Swagat Prasad Niroula <swagat.niroula@tum.de>
2023-04-01 10:21:27 +0200

    feat: get current cursor position

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Stage, Layer, Image } from 'react-konva';
 import useImage from 'use-image';
 
 import './Canvas.scss';
 
 const CanvasComponent = ({url}) => {
+  const [cursor, setCursor] = useState({ x: null, y: null });
+
   const windowWidth = window.innerWidth * 0.3;
 
   const LoadImage = () => {
     const [image] = useImage(url);
     return <Image image={image} />;
-  };  
+  };
+
+  const handleMouseMove = (event) => {
+    const stage = event.currentTarget;
+    setCursor(stage.getPointerPosition());
+  }
 
   return (
     <div className="canvas-container">
-      <Stage width={windowWidth} height={windowWidth}>
+      <Stage 
+        width={windowWidth}
+        height={windowWidth}
+        onMouseMove={handleMouseMove}
+        >
         <Layer>
           <LoadImage />
         </Layer>
