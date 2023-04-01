3aa4f2fcdeee663d6c95e3f7eb6defc39eeaa9f4
Swagat Prasad Niroula <swagat.niroula@tum.de>
2023-04-01 11:48:19 +0200

    feat: add basic logic to draw on canvas

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,33 +1,74 @@
-import { React, useState } from 'react';
-import { Stage, Layer, Image } from 'react-konva';
+import { React, useState, useRef } from 'react';
+import { Stage, Layer, Image, Line } from 'react-konva';
 import useImage from 'use-image';
 
 import './Canvas.scss';
 
 const CanvasComponent = ({url}) => {
-  const [cursor, setCursor] = useState({ x: null, y: null });
+  const [tool, setTool] = useState('brush');
+  const [lines, setLines] = useState([]);
+  const isDrawing = useRef(false);
 
   const windowWidth = window.innerWidth * 0.3;
 
   const LoadImage = () => {
     const [image] = useImage(url);
-    return <Image image={image} />;
+    return <Image image={image} width={windowWidth} height={windowWidth} />;
   };
 
-  const handleMouseMove = (event) => {
-    const stage = event.currentTarget;
-    setCursor(stage.getPointerPosition());
-  }
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
 
   return (
     <div className="canvas-container">
       <Stage 
         width={windowWidth}
         height={windowWidth}
-        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
         >
         <Layer>
           <LoadImage />
+        </Layer>
+        <Layer>
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#df4b26"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              lineJoin="round"
+              globalCompositeOperation={
+                line.tool === 'eraser' ? 'destination-out' : 'source-over'
+              }
+            />
+          ))}
         </Layer>
       </Stage>
     </div>
