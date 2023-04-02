50f2f33c51d501317e97058f2bae396ec738f832
Swagat Prasad Niroula <swagat.niroula@tum.de>
2023-04-02 11:25:47 +0200

    feat: track mouse position using state

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,14 +1,17 @@
-import { React, useState, useEffect, useRef } from 'react';
-import { Stage, Layer, Image, Line } from 'react-konva';
-import useImage from 'use-image';
+import { React, useState, useEffect, useRef } from "react";
+import Konva from "konva";
+import { Stage, Layer, Image, Line, Circle } from "react-konva";
+import useImage from "use-image";
 
-import './Canvas.scss';
-import ImageScaleHelper from '../../helpers/ImageScaleHelper';
+import "./Canvas.scss";
+import ImageScaleHelper from "../../helpers/ImageScaleHelper";
 
-const CanvasComponent = ({url, strokeWidth, toolType}) => {
+const CanvasComponent = ({ url, strokeWidth, toolType }) => {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
   const [lines, setLines] = useState([]);
   const isDrawing = useRef(false);
   const [image] = useImage(url);
+  const imageRef = useRef();
 
   useEffect(() => {
     if (!url) {
@@ -16,24 +19,47 @@ const CanvasComponent = ({url, strokeWidth, toolType}) => {
     }
   }, [url]);
 
+  // when image is loaded we need to cache the shape
+  useEffect(() => {
+    if (image) {
+      // you many need to reapply cache on some props changes like shadow, stroke, etc.
+      imageRef.current.cache();
+    }
+  }, [image]);
+
   const windowWidth = window.innerWidth * 0.3;
 
   const LoadImage = () => {
     if (image) {
       const canvasInfo = {
         width: windowWidth,
-        height: windowWidth
+        height: windowWidth,
       };
-  
+
       const imageInfo = {
         width: image.width,
         height: image.height,
       };
-  
-      const scaleInfo = ImageScaleHelper.scaleImage(canvasInfo, imageInfo, false);
-  
-      return <Image image={image} x={scaleInfo.x} y={scaleInfo.y} width={scaleInfo.width} height={scaleInfo.height} />;
-    } 
+
+      const scaleInfo = ImageScaleHelper.scaleImage(
+        canvasInfo,
+        imageInfo,
+        false
+      );
+
+      return (
+        <Image
+          ref={imageRef}
+          image={image}
+          x={scaleInfo.x}
+          y={scaleInfo.y}
+          width={scaleInfo.width}
+          height={scaleInfo.height}
+          filters={[Konva.Filters.Pixelate]}
+          pixelSize={10}
+        />
+      );
+    }
   };
 
   const handleMouseDown = (e) => {
@@ -41,8 +67,19 @@ const CanvasComponent = ({url, strokeWidth, toolType}) => {
       return;
     }
     isDrawing.current = true;
-    const pos = e.target.getStage().getPointerPosition();
-    setLines([...lines, { tool: toolType, points: [pos.x, pos.y], width: strokeWidth }]);
+    const mousePosition = e.target.getStage().getPointerPosition();
+    setMousePos({
+      x: mousePosition.x,
+      y: mousePosition.y,
+    });
+    setLines([
+      ...lines,
+      {
+        tool: toolType,
+        points: [mousePosition.x, mousePosition.y],
+        width: strokeWidth,
+      },
+    ]);
   };
 
   const handleMouseMove = (e) => {
@@ -51,10 +88,17 @@ const CanvasComponent = ({url, strokeWidth, toolType}) => {
       return;
     }
     const stage = e.target.getStage();
-    const point = stage.getPointerPosition();
+    const mousePosition = stage.getPointerPosition();
+    setMousePos({
+      x: mousePosition.x,
+      y: mousePosition.y,
+    });
     let lastLine = lines[lines.length - 1];
     // add point
-    lastLine.points = lastLine.points.concat([point.x, point.y]);
+    lastLine.points = lastLine.points.concat([
+      mousePosition.x,
+      mousePosition.y,
+    ]);
 
     // replace last
     lines.splice(lines.length - 1, 1, lastLine);
@@ -67,7 +111,7 @@ const CanvasComponent = ({url, strokeWidth, toolType}) => {
 
   return (
     <div className="canvas-container">
-      { !url && (
+      {!url && (
         <div className="text-container">
           Please upload an Image
           <br />
@@ -75,16 +119,14 @@ const CanvasComponent = ({url, strokeWidth, toolType}) => {
         </div>
       )}
       <div className="stage-container">
-        <Stage 
+        <Stage
           width={windowWidth}
           height={windowWidth}
           onMouseDown={handleMouseDown}
           onMousemove={handleMouseMove}
           onMouseup={handleMouseUp}
-          >
-          <Layer>
-            { url && <LoadImage /> }
-          </Layer>
+        >
+          <Layer>{url && <LoadImage />}</Layer>
           <Layer>
             {lines.map((line, i) => (
               <Line
@@ -93,19 +135,29 @@ const CanvasComponent = ({url, strokeWidth, toolType}) => {
                 stroke="#df4b26"
                 strokeWidth={line.width}
                 tension={0.5}
-                opacity={line.tool === 'eraser' ? 1 : 0.5}
+                opacity={line.tool === "eraser" ? 1 : 0.5}
                 lineCap="round"
                 lineJoin="round"
                 globalCompositeOperation={
-                  line.tool === 'eraser' ? 'destination-out' : 'source-over'
+                  line.tool === "eraser" ? "destination-out" : "source-over"
                 }
               />
             ))}
+          </Layer>
+          <Layer>
+            <Circle
+              x={mousePos.x}
+              y={mousePos.y}
+              radius={strokeWidth}
+              strokeWidth={0}
+              filters={[Konva.Filters.Pixelate]}
+              pixelSize={10}
+            />
           </Layer>
         </Stage>
       </div>
     </div>
   );
-}
+};
 
 export default CanvasComponent;
