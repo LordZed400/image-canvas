02ac8fdde3badec778230940b77f8c909c2a58c5
Swagat Prasad Niroula <swagat.niroula@tum.de>
2023-04-03 13:14:51 +0200

    feat: update assets for tool icons

---
+++ src/constants/imageSet.jsx
@@ -0,0 +1,10 @@
+export const set1 = require('./../assets/Images/Static/image-set-1.jpg');
+export const set2 = require('./../assets/Images/Static/image-set-2.jpg');
+export const set3 = require('./../assets/Images/Static/image-set-3.jpg');
+export const set4 = require('./../assets/Images/Static/image-set-4.jpg');
+export const set5 = require('./../assets/Images/Static/image-set-5.jpg');
+export const set6 = require('./../assets/Images/Static/image-set-6.jpg');
+export const set7 = require('./../assets/Images/Static/image-set-7.jpg');
+export const set8 = require('./../assets/Images/Static/image-set-8.jpg');
+
+export const imageSet = [set1, set2, set3, set4, set5, set6, set7, set8];
